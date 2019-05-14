6b9d12f2f0fdc673729ce06d12f0f8eb09f86f82
Evoletto <a.chrzastek.18@ucl.ac.uk>
2019-05-14 23:04:15 +0100

    Copy filtered files to another directory

--- FabFiltering.py
+++ FabFiltering.py
@@ -1,18 +1,15 @@
-# open file
-# my_file = open("1A2Y_1.pdb")
-# read the content
-# file_contents = my_file.read()
-# print(file_contents)
-
 import re
-import os
+import os, shutil
 directory_path = "/Users/Alina/Dropbox/Comp/NR_LH_Combined_Kabat/"
+ # get all the files in the listed directory
+ # all_files = sorted(os.listdir(directory_path))
 all_files = sorted(os.listdir(directory_path))
 
-
+# create a loop, go through file one line at a time
 for file in all_files:
-#tmp = "/Users/Alina/Dropbox/Comp/NR_LH_Combined_Kabat/1A3R_1.pdb"
+# tmp = "/Users/Alina/Dropbox/Comp/NR_LH_Combined_Kabat/1A3R_1.pdb"
 	filename = (directory_path + file)
+	# read line by line in the filename
 	lines = [line.rstrip('\n') for line in open(filename)]
 
 	conditions = ["L    L    A", 
@@ -21,24 +18,38 @@ for file in all_files:
 				  "H    H    H",
 				  "L    L    C",
 				  "H    H    D"]
-
+	conditions2 = ["A    N    N",
+				   "A    A    A",
+				   "A    P    P",
+				   "A    F    F"]
 
 	found = False	
 
+    # if it meets the conditions in line lines 7 to 10
 	for index, line in enumerate(lines, start=1):
 		# if index >= 7 and index < 10:
-
-		if index >= 7:
+		# if statement, conditions
+		if index >= 7 and index < 10:
+			# checks for positive conditions
 			for condition in conditions:
 				if line.find(condition) > 0:
 					found = True
-					break
-				if found == True:
-					break
-			if found == True:
-				# TODO: if found
-				if line.strip().endswith("CYS"):
-					print(filename)
 					break		
-					
-					
+			# checks for negative conditions
+			for condition in conditions2:
+				if line.find(condition) > 0:
+					found = False
+					break
+
+		if found == True:
+			# TODO: if found
+			if line.strip().endswith("CYS"):
+				shutil.copy(filename, "/Users/Alina/Dropbox/Comp/Fab filtered/")
+				print(filename)
+				break	
+
+
+
+
+
+
