18871f6b8f35ce57e89181279e83e76dcaf06fb7
Evoletto <a.chrzastek.18@ucl.ac.uk>
2019-05-17 11:28:50 +0100

    prevents reading hidden files

--- Fab_H_L_selection_whole_folder.py
+++ Fab_H_L_selection_whole_folder.py
@@ -7,7 +7,9 @@ directory_path = "/Users/Alina/Dropbox/Comp/NR_LH_Combined_Kabat/"
 all_files = sorted(os.listdir(directory_path))
 
 for index, file in enumerate(all_files, start=1):
-
+	if not file.endswith(".pdb"):
+		# prevents reading hidden files like .DS_Sore
+		continue
 	my_file = open(directory_path + file)
 	# read the content
 	file_contents = my_file.readlines()
@@ -18,9 +20,12 @@ for index, file in enumerate(all_files, start=1):
 	for line in file_contents:
 		if line.startswith("REMARK 950 CHAIN L    L"):
 			light_chain = line[len(line) - 2]
-		
+	
 		if line.startswith("REMARK 950 CHAIN H    H"):
 			heavy_chain = line[len(line) - 2]
 
 
 	print(file.rsplit('.', 1)[0], light_chain, heavy_chain)
+	
+	
+# code works
