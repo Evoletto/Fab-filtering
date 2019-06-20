066fd333eee723d987d7db78f0625199b78ad05d
Evoletto <a.chrzastek.18@ucl.ac.uk>
2019-06-20 10:22:51 +0100

    checks if PDB contains SSBOND and creates ZONE output

---
+++ Step_through_PDB_output.py
@@ -0,0 +1,93 @@
+import re
+import os, shutil
+import os.path
+import urllib.request
+
+def get_last_character(string):
+	return string[len(string) - 1:]
+
+def line_contains_the_same_conditions(line, conditions):
+	for condition in conditions:
+		if line.count(condition) > 1:
+			return True
+	return False	
+	
+	
+	
+directory_path = "/Users/Alina/Google_Drive_BBK/Comp/Compatible_SSBOND/"
+file1 = "1A3R_1.pdb"
+file2 = "1AD0_1.pdb"
+text_file = open("/Users/Alina/Google_Drive_BBK/Comp/Output.txt", "w")
+
+conditions = ["CYS L", "CYS H", "CYS A", "CYS B"]
+
+my_file = open(directory_path + file1)
+file_contents = my_file.readlines()
+
+res1 = []
+res2 = []
+
+for line in file_contents:
+	if line.strip().startswith("SSBOND   3 "):
+		if line_contains_the_same_conditions(line, conditions) == False:
+			for condition in conditions:
+				starts_at = line.find(condition)
+				if starts_at > 0:
+					stri = line[starts_at + len(condition):].strip()
+					number = stri[:3]
+					last = int(number) - 2
+					res1.append(get_last_character(condition) + str(last) + "-" + number) 		
+		else:
+ 			print("Contains the same conditions, file igonred")
+ 			
+other_file = open(directory_path + file2)
+file_contents = other_file.readlines()
+
+
+for line in file_contents:
+	if line.strip().startswith("SSBOND   3 "):
+		if line_contains_the_same_conditions(line, conditions) == False:
+			for condition in conditions:
+				starts_at = line.find(condition)
+				if starts_at > 0:
+					stri = line[starts_at + len(condition):].strip()
+					number = stri[:3]
+					last = int(number) - 2
+					res2.append(get_last_character(condition) + str(last) + "-" + number)		
+		else:
+ 			print("Contains the same conditions, file igonred")
+ 	
+
+if len(res1) < 2 or len(res2) < 2:
+	print("conditions not found")
+else:
+	text_file.write("ZONE " + res1[0] + ":" + res2[0])
+	text_file.write("\n")
+	text_file.write("ZONE " + res1[1] + ":" + res2[1])
+	text_file.write("\n")
+	text_file.write("FIT")
+	text_file.close()
+	
+# 	print("ZONE " + res1[0] + ":" + res2[0])
+# 	print("ZONE " + res1[1] + ":" + res2[1])
+
+
+
+
+# if not then ignore the file
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
