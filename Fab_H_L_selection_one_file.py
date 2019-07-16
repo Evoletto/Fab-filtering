2b0e0baa54b89e6a2aad546d8d5f13f2d79943dc
Evoletto <a.chrzastek.18@ucl.ac.uk>
2019-07-16 10:22:59 +0100

    to be removed

--- Fab_H_L_selection_one_file.py
+++ Fab_H_L_selection_one_file.py
@@ -4,7 +4,7 @@ import os.path
 
 
 directory_path = "/Users/Alina/Dropbox/Comp/NR_LH_Combined_Kabat/"
-file = "1A3R_1.pdb"
+file = "8FAB_1.pdb"
 
 my_file = open(directory_path + file)
 # read the content
@@ -21,12 +21,8 @@ for line in file_contents:
 	if line.startswith("REMARK 950 CHAIN H    H"):
 		striped = line.strip()
 		heavy_chain = striped[len(striped) - 1]
-		
-	if line.strip().endswith("CYS"):
-		print ("LC ends on " + line.strip()[-3:])
+
 
 print(file.rsplit('.', 1)[0], light_chain, heavy_chain)	
 
 
-
-# this code works
