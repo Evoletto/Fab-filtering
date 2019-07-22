ef5364f92539e9a1a7803d0ced3d7d9de4d1ae65
Evoletto <a.chrzastek.18@ucl.ac.uk>
2019-07-22 13:28:38 +0100

    residue number to be the same

--- chain_format.py
+++ chain_format.py
@@ -22,131 +22,91 @@
 #   EMail:      a.chrzastek.18@ucl.ac.uk
 #*************************************************************************
 
-import re
-import os, shutil
-import os.path
-import urllib.request
-import sys
+import os
 
-#source = "/Volumes/ALINASD/PhD_Rotations/Comp/VMD_vis/results_for_VMD_replaced.pdb"#sys.argv[1]
-source = sys.argv[1]
-my_file = open(source)
-file_contents = my_file.readlines()
+for file_index in range(1, 11):
+	# loop from 1 to 10, 11 not included
+	source = "/Volumes/ALINASD/PhD_Rotations/Comp/VMD_vis/results_for_VMD_v" + str(file_index) + ".pdb" 
+	#source = sys.argv[1]
+	my_file = open(source)
+	file_contents = my_file.readlines()
 
 
-text_file_31 = open("results_for_VMD_31_lines.pdb", "w")
-text_file_35 = open("results_for_VMD_35_lines.pdb", "w")
+	# text_file_31 = open("results_for_VMD_31_lines.pdb", "w")
+
+	def get_chain_numbers(model):
+		numbers = []
+		for line in model:
+			if line.startswith("ATOM"):
+				number = line[22:26]
+				numbers.append(number)
+		return numbers
+
+	# modifies each model - changing chain labels, atom number
+	replaced_model_lines = []
+	grouped_models = {}
+	single_model_lines = []
 
 
-def get_last_character(string):
-	return string[len(string) - 1:]
-
-# replacing string with replacementString in the array
-def replaceStringInArray(array, string, replacementString):
-	new_array = []
-	for element in array:
-		if element == string:
-			new_array.append(replacementString)
-		else:
-			new_array.append(element)
-	return new_array
-
-# array's element at given index
-def replace_element_in_array(in_ar, repl, i):
-	if len(in_ar) > (i):
-		new_array = in_ar
-		new_array[i] = repl
-		return new_array
-	else:
-		return in_ar
-
-# append 'to_array' with value at given index in 'from_array'
-def append_element_to_array(from_array, to_array, at_index):
-	if len(from_array) > at_index - 1:
-		label = from_array[at_index]
-		new_ar = to_array
-		new_ar.append(label)
-		return new_ar
-	else:
-		return to_array
-
-# formats array elements into string respecting pdb file format spacing
-def array_to_pdb_line(array):
-	combined = ""	
-	spacing = ["<6", ">5", "<4", ">3", "<1", "<6", ">8", ">8", ">8", ">6", ">1"]
-	for index, u in enumerate(array, start=0):
-		combined = combined + ("{:" + str(spacing[index]) + "}").format(u.strip())
-		if index == 1:
-			combined = combined + "  " 
-		if index == 3 or index == 4 or index == 9:
-			combined = combined + " "
-	return combined + "\n"
-	
-				
-grouped_models = {}
-single_model_lines = []
-current_model_number = ""
-# creates hash table from file containing all models
-for index, line in enumerate(file_contents, start=0):
-	stripped_line = line.strip()
-	single_model_lines.append(line)
-	if stripped_line.startswith("MODEL"):
-		model_number = (stripped_line[stripped_line.find("MODEL") + len("MODEL"):]).replace(" ", "")
-		current_model_number = str(model_number)
-	elif stripped_line.startswith("ENDMDL"):
-		# copy that array into grouped models hash table at given model number
-		grouped_models[current_model_number] = single_model_lines
-		single_model_lines = []
-
-chain_numbers_31 = []
-chain_numbers_35 = []
-chain_labels_31 = []
-chain_labels_35 = []
-
-# modifies each model - changing chain labels, atom number
-for model_key in grouped_models:
-	model_line_no = len(grouped_models[model_key])
-
-	for index, model_line in enumerate(grouped_models[model_key], start=0):
-		line_into_array = list(filter(lambda a: a != '', model_line.split(" ")))
-		last = line_into_array[len(line_into_array)-1]
-		if last.startswith("1.00"):
-			line_into_array = line_into_array[:len(line_into_array) - 1]
-			line_into_array.append("1.00")
-			line_into_array.append(last[4:])
-		if line_into_array[0] == "ATOM":
-			line_into_array[1] = str(index - 1)
-			
-		line_into_array = replaceStringInArray(line_into_array, "A", "L")
-		line_into_array = replaceStringInArray(line_into_array, "B", "H")
-
-
-		if model_line_no == 31:
-			if len(chain_numbers_31) < 29:
-				# creates chain label array that is used for all models that consists of 31 lines
-				# from that array, chain number and label is replaced
-				chain_numbers_31 = append_element_to_array(line_into_array, chain_numbers_31, 5)
-				chain_labels_31 = append_element_to_array(line_into_array, chain_labels_31, 3)
-		
-			if index > 1 and index < 30:
-				line_into_array = replace_element_in_array(line_into_array, chain_numbers_31[index - 2], 5)
-				line_into_array = replace_element_in_array(line_into_array, chain_labels_31[index - 2], 3)
-				text_file_31.write(array_to_pdb_line(line_into_array))
+	# creates hash table from file containing all models
+	model_line_count = 0
+	for index, line in enumerate(file_contents, start=0):
+		model_line_count += 1
+		stripped_line = line.strip()
+		single_model_lines.append(line)
+		if stripped_line.startswith("MODEL"):
+			model_number = (stripped_line[stripped_line.find("MODEL") + len("MODEL"):]).replace(" ", "")
+			current_model_number = str(model_number)
+		elif stripped_line.startswith("ENDMDL"):
+			# copy that array into grouped models hash table at given model number
+	# 		print(":", grouped_models[model_line_count])
+			if grouped_models.get(model_line_count):
+				grouped_models[model_line_count].append(single_model_lines)
 			else:
-				text_file_31.write(array_to_pdb_line(line_into_array))
-		if model_line_no == 35:
-			if len(chain_numbers_35) < 33:
-				chain_numbers_35 = append_element_to_array(line_into_array, chain_numbers_35, 5)
-				chain_labels_35 = append_element_to_array(line_into_array, chain_labels_35, 3)
-				
-			if index > 1 and index < 34:
-				line_into_array = replace_element_in_array(line_into_array, chain_numbers_35[index - 2], 5)
-				line_into_array = replace_element_in_array(line_into_array, chain_labels_35[index - 2], 3)
-				text_file_35.write(array_to_pdb_line(line_into_array))
-			else:
-				text_file_35.write(array_to_pdb_line(line_into_array))
+				grouped_models[model_line_count] = [single_model_lines]		
+			single_model_lines = []
+			model_line_count = 0
+
+	# for line_count in grouped_models:
+	# 	print(line_count, ":")
+	# 	for l in grouped_models[line_count]:
+	# 		for i in l:
+	# 			print(i)
 		
-text_file_31.close()
-text_file_35.close()
+	# modifies each model - changing chain labels, atom number
+	mod = []
+	for line_count in grouped_models:
+		base_model = grouped_models[line_count][0]
+	# 	print("base model for ", line_count, " : ", base_model)
+		for models in grouped_models[line_count]:
+			# models for this line len
 
+	# 		k = list(grouped_models.keys())[0]
+		
+			base_chain_numbers = get_chain_numbers(base_model)
+			new_model_lines = []
+			for index, model_line in enumerate(models,start=0):
+	# 			for index, model_line in enumerate(grouped_models[model_key], start=0):
+				if model_line.startswith("ATOM"):
+					# replace numbers
+					# check if valid index - number of lines
+					new_line = model_line[:22] + str(base_chain_numbers[index - 2]) + model_line[26:]
+					new_model_lines.append(new_line)
+				else:
+					# copy current line without modifications
+					new_model_lines.append(model_line)
+			mod.append(new_model_lines)
+		break
 
+	os.remove(source) #this deletes the file
+	# new file
+	pdb_file = open(source, "w")
+	for model in mod:
+		for model_line in model:
+			#print(ln)
+			pdb_file.write(model_line)
+	pdb_file.close()
+	# 
+	# for m in mod:
+	# 	for n in mod[m]:
+	# 		print(n)
