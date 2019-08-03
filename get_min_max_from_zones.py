a46d255b54299b25a36102e2684cae1414f43207
Evoletto <a.chrzastek.18@ucl.ac.uk>
2019-08-03 19:01:23 +0100

    authorship added

--- get_min_max_from_zones.py
+++ get_min_max_from_zones.py
@@ -1,6 +1,6 @@
 #*************************************************************************
 #
-#   Program:    get_min_max_from_zones
+#   Program:    Uses pdbfindnearres to find residue of intrest 
 #   File:       get_min_max_from_zones.py
 #   
 #   Version:    V1.0
@@ -20,10 +20,8 @@
 #               WC1E 6BT
 #   EMail:      a.chrzastek.18@ucl.ac.uk
 #*************************************************************************
-
-
 import os.path
-import sys
+import sys 
 from subprocess import call
 
 def get_first_character(string):
@@ -35,25 +33,45 @@ def split_string_by(string, delimiter):
 		first_part = string[:delimiter_index]
 		second_part = string[delimiter_index + 1:]
 		return [first_part, second_part]
+	else:
+		return [string]
 
 def get_last_character(string):
 	return string[len(string) - 1:]
 
+def get_last_n_character(n, string):
+	return string[len(string) - n:]
+
 def get_zones_from_string(zone):
-	#'L211-L213'
 	split_zone = split_string_by(zone, "-")
-	if len(split_zone) > 0:
+	if len(split_zone) == 1:
+		# short zones (single chain)
 		first = split_zone[0]
-		last_digit = int(get_last_character(first))
-		copied = first[:len(first) - 1] + (str(last_digit + 1))
-		split_zone.insert(1, copied)
+		last_digit = int(get_last_n_character(2, first))
+		second = first[:len(first) - 2] + (str(last_digit - 1))
+		split_zone.insert(1, second)
+		third = ""
+		third_end = last_digit - 2
+		if third_end < 10 :
+			third = first[:len(first) - 2] + "0" + (str(third_end))
+		else:
+			third = first[:len(first) - 2] + (str(third_end))
+		split_zone.insert(2, third)
 		return split_zone
+	else:
+		# old way, zones
+		#'L211-L213'
+		if len(split_zone) > 0:
+			first = split_zone[0]
+			last_digit = int(get_last_character(first))
+			copied = first[:len(first) - 1] + (str(last_digit + 1))
+			split_zone.insert(1, copied)
+			return split_zone
 
 if len(sys.argv) > 1:
 	find_area_array = sys.argv[1].split("\n")
 	zones = sys.argv[2]
 	if len(find_area_array) > 0:
-	
 	
 		split_zones = split_string_by(zones, ",")
 		if len(split_zones) > 1:		
@@ -80,8 +98,9 @@ if len(sys.argv) > 1:
 				chain = second_chain + str(second_chain_max)
 				res.append(chain)
 			for zone in split_zones:
+				
 				res.extend(get_zones_from_string(zone))
 			
-			print(",".join(res))
+			print(",".join(set(res)))
 
 		
