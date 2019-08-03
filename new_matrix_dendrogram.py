1bad8de4a18916f5f8bb4d25037046082c8adeeb
Evoletto <a.chrzastek.18@ucl.ac.uk>
2019-08-03 18:15:15 +0100

    Produces a dendrogram based on the RMSDs data

---
+++ new_matrix_dendrogram.py
@@ -0,0 +1,114 @@
+#*************************************************************************
+#
+#   Program:    Hierarchal Clustering plot and Fancy Dendrogram
+#   File:      	new_matrix_2.py
+#   
+#   Version:    V1.0
+#   Date:       27.06.19
+#   Function:   Runs through the RMSDs.txt result file. 
+#		 		Generates matrix and based on that produces figures.
+#
+#   
+#   Copyright:  (c) Alina Chrzastek, UCL, 2019
+#   Author:     Alina Chrzastek
+#   Address:    Institute of Structural and Molecular Biology
+#               Division of Biosciences
+#               University College
+#               Gower Street
+#               London
+#               WC1E 6BT
+#   EMail:      a.chrzastek.18@ucl.ac.uk
+#*************************************************************************
+import os.path
+import sys
+import numpy as np
+import matplotlib.pyplot as plt
+import plotly
+from scipy.cluster.hierarchy import dendrogram, linkage  
+from matplotlib import pyplot as plt
+
+#rmsd_file_path = "/Volumes/ALINASD/PhD_Rotations/Comp/Graphs_Figures/RMSDs_4.txt" #sys.argv[1]
+#rmsd_file_path = "/Volumes/ALINASD/PhD_Rotations/Comp/Graphs_Figures/RMSDs_3L5X_1_against_all.txt"
+#rmsd_file_path = sys.argv[1]
+
+
+rmsd_file = open(rmsd_file_path)
+file_contents = rmsd_file.readlines()
+matrix = []
+current_file = ""
+for line in file_contents:
+	# get first file name
+	delimiter = line.find("-")
+	first_filename = line[:delimiter].replace(".pdb", "")
+	# get rms value
+	rms_value = float(line[line.find("RMS:") + len("RMS:"):])
+	# create array with those values
+	if current_file == "":
+		current_file = first_filename
+		matrix.append([rms_value])
+		
+	elif current_file == first_filename:
+		matrix[-1].append(rms_value)
+	else: 
+		#new row
+		matrix.append([rms_value])
+		
+	current_file = first_filename
+	
+X = np.array(matrix)
+print(X)
+
+ #Hierarchal Clustering plot
+# labels = range(1, 165)  
+# plt.figure(figsize=(10, 7))  
+# plt.subplots_adjust(bottom=0.1)  
+# plt.scatter(X[:,0],X[:,1], label='True Position')
+# #plt.title('Hierarchical Clustering Dendrogram (truncated)')
+# plt.xlabel('sample index')
+# plt.ylabel('distance')
+
+# for label, x, y in zip(labels, X[:, 0], X[:, 1]):  
+#     plt.annotate(
+#         label,
+#         xy=(x, y), xytext=(-3, 3),
+#         textcoords='offset points', ha='right', va='bottom')
+# plt.show()
+
+Z = linkage(X, 'ward')
+
+# Fancy Dendrogram
+def fancy_dendrogram(*args, **kwargs):
+    max_d = kwargs.pop('max_d', None)
+    if max_d and 'color_threshold' not in kwargs:
+        kwargs['color_threshold'] = max_d
+    annotate_above = kwargs.pop('annotate_above', 0)
+
+    ddata = dendrogram(*args, **kwargs)
+
+    if not kwargs.get('no_plot', False):
+        plt.title('Hierarchical Clustering Dendrogram (truncated)')
+        plt.xlabel('sample index or (cluster size)')
+        plt.ylabel('distance')
+        for i, d, c in zip(ddata['icoord'], ddata['dcoord'], ddata['color_list']):
+            x = 0.5 * sum(i[1:3])
+            y = d[1]
+            if y > annotate_above:
+                plt.plot(x, y, 'o', c=c)
+                plt.annotate("%.3g" % y, (x, y), xytext=(0, -5),
+                             textcoords='offset points',
+                             va='top', ha='center')
+        if max_d:
+            plt.axhline(y=max_d, c='k')
+    return ddata
+#     
+fancy_dendrogram(
+    Z,
+    truncate_mode='lastp',
+    p=12,
+    leaf_rotation=90.,
+    leaf_font_size=12.,
+    show_contracted=True,
+    annotate_above=10,  # useful in small plots so annotations don't overlap
+)
+plt.show()
+
